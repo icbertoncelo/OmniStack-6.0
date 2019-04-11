45e43354ae9d6ca7c233f0cbdb25cce635b36061
Ian Carlos <carlos.ian007@gmail.com>
2019-04-10 23:49:55 -0300

    Finishing Client Side

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-import Main from "./pages/Main";
+import Routes from "./routes";
 
-class App extends Component {
-  render() {
-    return <Main />;
-  }
-}
+const App = () => <Routes />;
 
 export default App;
--- client/src/pages/Main/index.js
+++ client/src/pages/Main/index.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 import "./styles.css";
 import logo from "../../assets/logo.svg";
+import api from "../../services/api";
 
 export default class Main extends Component {
+  state = {
+    newBox: ""
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const response = await api.post("boxes", {
+      title: this.state.newBox
+    });
+
+    this.props.history.push(`/box/${response.data._id}`);
+  };
+
+  handleInputChange = e => {
+    this.setState({ newBox: e.target.value });
+  };
+
   render() {
     return (
       <div>
         <div id="main-container">
-          <form action="">
+          <form onSubmit={this.handleSubmit}>
             <img src={logo} alt="" />
-            <input placeholder="Create a Box" />
+            <input
+              placeholder="Create a Box"
+              value={this.state.newBox}
+              onChange={this.handleInputChange}
+            />
             <button type="submit">Create</button>
           </form>
         </div>
