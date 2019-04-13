35ab5296445d85d60ef5be93f504a4767440d1f7
Ian Carlos <carlos.ian007@gmail.com>
2019-04-13 11:08:39 -0300

    Adding Mobile Interface and Functionality

--- mobile/src/pages/Box/index.js
+++ mobile/src/pages/Box/index.js
@@ -1,9 +1,111 @@
 import React, { Component } from "react";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import styles from "./styles";
+import AsyncStorage from "@react-native-community/async-storage";
+import ImagePicker from "react-native-image-picker";
+import RNFS from "react-native-fs";
+import FileViewer from "react-native-file-viewer";
+import socket from "socket.io-client";
 
-import { View } from "react-native";
+import api from "../../services/api";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import { distanceInWords } from "date-fns";
+import pt from "date-fns/locale/pt";
 
 export default class Box extends Component {
+  state = {
+    box: {}
+  };
+
+  openFile = async file => {
+    try {
+      const filePath = `${RNFS.DocumentDirectoryPath}/${file.title}`;
+
+      await RNFS.downloadFile({
+        formUrl: file.url,
+        toFile: filePath
+      });
+
+      await FileViewer.open(filePath);
+    } catch (err) {
+      console.log("Arquivo nao suportado");
+    }
+  };
+
+  async componentDidMount() {
+    const box = await AsyncStorage.getItem("@RocketBox:box");
+    this.subscribeToNewFiles(box);
+
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({ box: response.data });
+  }
+
+  subscribeToNewFiles = box => {
+    const io = socket("https://omni-stack-server.herokuapp.com");
+
+    io.emit("connectRoom", box);
+    io.on("file", data => {
+      this.setState({
+        box: { ...this.state.box, files: [data, ...this.state.box.files] }
+      });
+    });
+  };
+
+  renderItem = ({ item }) => (
+    <TouchableOpacity style={styles.file} onPress={() => {}}>
+      <View style={styles.fileInfo}>
+        <Icon name="insert-drive-file" size={24} color="#A5CFFF" />
+        <Text style={styles.fileTitle}>{item.title}</Text>
+      </View>
+
+      <Text style={styles.fileData}>
+        Há {distanceInWords(item.createdAt, new Date(), { locale: pt })}
+      </Text>
+    </TouchableOpacity>
+  );
+
+  handleUpload = () => {
+    ImagePicker.launchImageLibrary({}, async upload => {
+      if (upload.error) {
+        console.log("Error");
+      } else if (upload.didCancel) {
+        console.log("Canceled ");
+      } else {
+        const data = new FormData();
+
+        const [prefix, suffix] = upload.fileName.split(".");
+        const ext = suffix.toLowerCase() === "heic" ? "jpg" : suffix;
+
+        data.append("file", {
+          uri: upload.uri,
+          type: upload.type,
+          name: `${prefix}.${ext}`
+        });
+
+        api.post(`boxes/${this.state.box._id}/files`, data);
+        console.log(this.state.box._id);
+      }
+    });
+  };
+
   render() {
-    return <View />;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.boxTitle}>{this.state.box.title}</Text>
+        <FlatList
+          style={styles.list}
+          data={this.state.box.files}
+          keyExtractor={file => file._id}
+          ItemSeparatorComponent={() => <View style={styles.separator} />}
+          renderItem={this.renderItem}
+        />
+
+        <TouchableOpacity style={styles.fab} onPress={this.handleUpload}>
+          <Icon name="cloud-upload" size={24} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    );
   }
 }
